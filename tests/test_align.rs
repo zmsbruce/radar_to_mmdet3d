18d7d954b58deb19eb2667d8ae05891ffb72c948
zmsbruce <zmsbruce@163.com>
2024-11-08 05:13:47 +0000

    test: :white_check_mark: Update tests
    Update tests of 98bf67a270f9d79fd4f8d5c1c7437ea518815713

--- tests/test_align.rs
+++ tests/test_align.rs
@@ -21,6 +21,24 @@ fn test_get_video_frame_num() -> Result<(), Box<dyn Error>> {
 }
 
 #[test]
+fn test_get_video_frame_num_nonexistent_path() -> Result<(), Box<dyn Error>> {
+    let nonexistent_path = PathBuf::from("nonexistent_video.mp4");
+
+    let result = get_video_frame_num(
+        nonexistent_path
+            .to_str()
+            .ok_or("Failed to convert path to string")?,
+    );
+
+    assert!(
+        result.is_err(),
+        "The function should return an error when the video file does not exist"
+    );
+
+    Ok(())
+}
+
+#[test]
 fn test_get_rosbag_frame_num() -> Result<(), Box<dyn Error>> {
     let filename = "tests/test.bag"; // A bag with 10 frames of /lidar and 8 frames of /camera
     let lidar_topic = "/lidar";
@@ -28,6 +46,22 @@ fn test_get_rosbag_frame_num() -> Result<(), Box<dyn Error>> {
 
     let frame_count = get_rosbag_frame_num(filename, lidar_topic, video_topic)?;
     assert_eq!(frame_count, 8, "Frame count should be 10");
+
+    Ok(())
+}
+
+#[test]
+fn test_get_rosbag_frame_num_nonexistent_path() -> Result<(), Box<dyn Error>> {
+    let nonexistent_bag_path = "nonexistent.bag";
+    let lidar_topic = "/lidar";
+    let video_topic = "/camera";
+
+    let result = get_rosbag_frame_num(nonexistent_bag_path, lidar_topic, video_topic);
+
+    assert!(
+        result.is_err(),
+        "The function should return an error when the ROS bag file does not exist"
+    );
 
     Ok(())
 }
