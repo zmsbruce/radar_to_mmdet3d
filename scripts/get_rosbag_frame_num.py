40e72921966be3c8fbefb374ca299117d858dc5d
zmsbruce <zmsbruce@163.com>
2024-11-09 06:33:21 +0000

    chore: :truck: Move scripts and assets

--- scripts/get_rosbag_frame_num.py
+++ scripts/get_rosbag_frame_num.py
@@ -1,13 +1,12 @@
 import rosbag
 
 
-def get_rosbag_frame_num(filename, lidar_topic, video_topic):
+def get_rosbag_frame_num(filename, lidar_topic):
     bag = rosbag.Bag(filename)
-    frame_count_list = []
-    for topic in [lidar_topic, video_topic]:
-        frame_count = 0
-        for topic, _, _ in bag.read_messages(topics=[topic]):
-            frame_count += 1
-        frame_count_list.append(frame_count)
+
+    frame_count = 0
+    for _, _, _ in bag.read_messages(topics=[lidar_topic]):
+        frame_count += 1
+        
     bag.close()
-    return min(frame_count_list)
+    return frame_count
