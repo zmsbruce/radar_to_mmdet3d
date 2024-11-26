90d54ec82ba3d6a7b3b70bcc4978103403ac2df3
zmsbruce <zmsbruce@163.com>
2024-11-26 13:40:10 +0800

    feat: :sparkles: Update async image saving

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,3 +1,31 @@
+use std::{io::Cursor, path::PathBuf};
+
+use anyhow::Result;
+use image::DynamicImage;
+use tokio::{fs::File, io::AsyncWriteExt};
+use tracing::error;
+
 pub mod hdf5;
 pub mod pcd;
 pub mod video;
+
+pub async fn save_image(image: DynamicImage, path: PathBuf) -> Result<()> {
+    let mut buffer = Cursor::new(Vec::new());
+    let format = image::ImageFormat::Png;
+
+    image.write_to(&mut buffer, format).map_err(|e| {
+        error!("Failed to write image to buffer: {e}");
+        e
+    })?;
+
+    let mut file = File::create(path).await.map_err(|e| {
+        error!("Failed to create image file: {e}");
+        e
+    })?;
+    file.write_all(&buffer.into_inner()).await.map_err(|e| {
+        error!("Failed to write image buffer to file");
+        e
+    })?;
+
+    Ok(())
+}
