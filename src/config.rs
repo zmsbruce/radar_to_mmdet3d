07b206fc39e580d2876f56ae998c62b0f6e45adf
zmsbruce <zmsbruce@163.com>
2024-11-26 13:42:04 +0800

    feat: :wrench: Update configurations

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,8 @@
 use std::fs;
 
-use anyhow::{Context, Result};
+use anyhow::Result;
 use serde::Deserialize;
-use tracing::{debug, span, trace, Level};
+use tracing::{debug, error, span, trace, Level};
 
 #[derive(Debug, Deserialize)]
 pub struct RadarConfig {
@@ -47,12 +47,53 @@ impl RadarConfig {
         let _enter = span.enter();
 
         trace!("Reading content from file {:?}...", file_path);
-        let config_content =
-            fs::read_to_string(file_path).context("Failed to read config from file")?;
+        let config_content = fs::read_to_string(file_path).map_err(|e| {
+            error!("Failed to read radar config from file: {e}");
+            e
+        })?;
 
         trace!("Deserializing content to RadarConfig...");
-        let config: Self = toml::from_str(&config_content)
-            .context("Failed to deserialize content to RadarConfig")?;
+        let config: Self = toml::from_str(&config_content).map_err(|e| {
+            error!("Failed to parse radar config: {e}");
+            e
+        })?;
+
+        debug!("Configurations: {:#?}", config);
+        Ok(config)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct SourceConfig {
+    pub video: Vec<VideoSourceConfig>,
+    pub point_cloud_file_path: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct VideoSourceConfig {
+    pub name: String,
+    pub file_path: String,
+}
+
+impl SourceConfig {
+    pub fn from_file<P>(file_path: P) -> Result<Self>
+    where
+        P: AsRef<std::path::Path> + std::fmt::Debug,
+    {
+        let span = span!(Level::TRACE, "SourceConfig::from_file");
+        let _enter = span.enter();
+
+        trace!("Reading content from file {:?}...", file_path);
+        let config_content = fs::read_to_string(file_path).map_err(|e| {
+            error!("Failed to read source config from file: {e}");
+            e
+        })?;
+
+        trace!("Deserializing content to SourceConfig...");
+        let config: Self = toml::from_str(&config_content).map_err(|e| {
+            error!("Failed to parse source config: {e}");
+            e
+        })?;
 
         debug!("Configurations: {:#?}", config);
         Ok(config)
